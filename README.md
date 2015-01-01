cbe39eefb938e809d785d5962187b0176f4ceac9
Psi <psi.xie@gmail.com>
2015-01-01 21:25:21 +0800

    支持 OpenCC 1.0.2

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ ropencc is a project for conversion between Traditional and Simplified Chinese, 
 还有若干语言习惯的问题，例如同一个地名的称呼就不一样，就不一一列举了
 
 而 Open Chinese Convert（OpenCC）「开放中文转换」，是一个致力于中
-文简繁转换的项目，由清华大学的 [BYVoid](http://www.byvoid.com/blog/about/) 提供高质量词库和函数库(libopencc)。<a href='http://code.google.com/p/opencc/'>更多介紹</a>。
+文简繁转换的项目，由清华大学的 [BYVoid](http://www.byvoid.com/blog/about/) 提供高质量词库和函数库(libopencc)。<a href='https://github.com/BYVoid/OpenCC'>更多介紹</a>。
 
 Install
 -------
@@ -37,33 +37,45 @@ Install
 
         brew install opencc
 
-    更多 libopencc 安装介绍请查看： <http://code.google.com/p/opencc/wiki/Install>
+    更多 libopencc 安装介绍请查看： <https://github.com/BYVoid/OpenCC>
 
 2. 安装 ropencc
-   
+
         gem install ropencc
+
+预设配置文件
+-----
+
+* `s2t.json` Simplified Chinese to Traditional Chinese 簡體到繁體
+* `t2s.json` Traditional Chinese to Simplified Chinese 繁體到簡體
+* `s2tw.json` Simplified Chinese to Traditional Chinese (Taiwan Standard) 簡體到臺灣正體
+* `tw2s.json` Traditional Chinese (Taiwan Standard) to Simplified Chinese 臺灣正體到簡體
+* `s2hk.json` Simplified Chinese to Traditional Chinese (Hong Kong Standard) 簡體到香港繁體（香港小學學習字詞表標準）
+* `hk2s.json` Traditional Chinese (Hong Kong Standard) to Simplified Chinese 香港繁體（香港小學學習字詞表標準）到簡體
+* `s2twp.json` Simplified Chinese to Traditional Chinese (Taiwan Standard) with Taiwanese idiom 簡體到繁體（臺灣正體標準）並轉換爲臺灣常用詞彙
+* `tw2sp.json` Traditional Chinese (Taiwan Standard) to Simplified Chinese with Mainland Chinese idiom 繁體（臺灣正體標準）到簡體並轉換爲中國大陸常用詞彙
 
 Usage
 -----
 
 简转繁 (Simple Interface)
 
-    Ropencc.conv('simp_to_trad', str)
+    Ropencc.conv('s2t.json', str)
 
 繁转简 (Simple Interface)
 
-    Ropencc.conv('trad_to_simp', str)
+    Ropencc.conv('t2s.json', str)
 
 简转繁 (File-Class like Interface)
 
-    Ropencc.open 'zhs2zhtw_vp.ini' do |cc|
-        rs = cc.convert '新年快乐''
+    Ropencc.open 's2t.json' do |cc|
+        rs = cc.convert '新年快乐'
         assert_equal '新年快樂', rs
     end
 
 繁转简 (File-Class like Interface)
 
-    Ropencc.open 'zhtw2zhcn_s.ini' do |cc|
+    Ropencc.open 't2s.json' do |cc|
         rs = cc.convert '新年快樂'
         assert_equal '新年快乐', rs
     end
--- lib/ropencc.rb
+++ lib/ropencc.rb
@@ -8,28 +8,13 @@ class Ropencc
         ffi_lib 'opencc'
         attach_function :opencc_open, [:string], :pointer
         attach_function :opencc_close, [:pointer], :int
+        attach_function :opencc_convert_utf8_to_buffer, [:pointer, :string, :int, :pointer], :int
         attach_function :opencc_convert_utf8, [:pointer, :string, :int], :pointer
-        attach_function :opencc_convert, [:pointer, :string, :int], :int
-        attach_function :opencc_perror, [:string], :void
-        attach_function :opencc_dict_load, [:pointer, :string, :int], :int
+        attach_function :opencc_convert_utf8_free, [:pointer], :void
+        attach_function :opencc_error, [], :string
     end
 
-    module LibC
-        extend FFI::Library
-        ffi_lib FFI::Library::LIBC
-        attach_function :free, [:pointer], :void
-    end # module LibC
-
     attr_accessor :descriptor
-
-    CONFIGS = ['zhs2zhtw_p.ini', 'zhs2zhtw_v.ini', 'zhs2zhtw_vp.ini', 'zht2zhtw_p.ini', 
-               'zht2zhtw_v.ini', 'zht2zhtw_vp.ini', 'zhtw2zhs.ini', 'zhtw2zht.ini', 
-               'zhtw2zhcn_s.ini', 'zhtw2zhcn_t.ini', 'zhs2zht.ini', 'zht2zhs.ini']
-
-    DICTS = {
-        :simp_to_trad => 'zhs2zhtw_vp.ini',
-        :trad_to_simp => 'zhtw2zhcn_s.ini'
-    }
 
     def initialize(config_file)
         @descriptor = LibOpenCC.opencc_open config_file
@@ -54,26 +39,23 @@ class Ropencc
         end
     end
 
-    def self.conv(dicttype, str)
-        raise 'unknown simple conversion type' if DICTS.has_key?(dicttype)
-        od = LibOpenCC.opencc_open DICTS[dicttype.to_sym]
+    def self.conv(config, str)
+        od = LibOpenCC.opencc_open config
         ptr = LibOpenCC.opencc_convert_utf8(od, str, str.bytesize)
         out_str = ptr.read_string
-        LibC.free ptr
+        LibOpenCC.opencc_convert_utf8_free ptr
         LibOpenCC.opencc_close od
         out_str.force_encoding("UTF-8") if out_str.respond_to?(:force_encoding)
         out_str
-
     end
 
     def convert(str)
         if @is_open == false
             raise IOError, 'not opened for conversion'
         end
-
         ptr = LibOpenCC.opencc_convert_utf8(@descriptor, str, str.bytesize)
         out_str = ptr.read_string
-        LibC.free ptr
+        LibOpenCC.opencc_convert_utf8_free ptr
         out_str.force_encoding("UTF-8") if out_str.respond_to?(:force_encoding)
         out_str
     end
@@ -87,7 +69,6 @@ class Ropencc
         LibOpenCC.opencc_close @descriptor
         @descriptor = nil
         @is_open = false
-
         return nil
     end
 end
